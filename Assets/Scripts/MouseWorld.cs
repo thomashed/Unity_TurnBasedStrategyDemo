99ab0463c837bb41bd6e2de7b5acae0cfa092224
Thomas <thomashj93@gmail.com>
2022-11-17 20:56:41 +0100

    add barebone script files

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    /// <summary>
+    /// mouse click register
+    /// publish event that primary input was clicked
+    /// cursorobject goes where mouseclicked
+    /// </summary>
+
+    void Update()
+    {
+            
+    }
+}
