0bf94facb62009240a060763e56b55a40e7e755f
Thomas <thomashj93@gmail.com>
2022-12-02 08:35:07 +0800

    stage interface starting structure

---
+++ Assets/Scripts/Interfaces/ITesting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolBeans.Interfaces
+{
+    internal interface ITesting
+    {
+        public int Foo();
+    }
+}
