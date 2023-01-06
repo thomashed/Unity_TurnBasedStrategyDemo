95220cd2e64359b99e8dc4d626f3c60304e15682
Thomas <thomashj93@gmail.com>
2023-01-06 11:16:33 +0100

    Forgot to stage: Add button and event to trigger and be shown if we have an active action to cover action UI

---
+++ Assets/Scripts/ActionBusyUI.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionBusyUI : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        UnitActionSystem.Instance.BusyChanged += UnitActionSystem_OnBusyChanged;
+        gameObject.SetActive(false); // hide busy button by default
+    }
+
+    private void UnitActionSystem_OnBusyChanged(object sender, bool isBusy)
+    {
+        print("event: " + isBusy);
+        gameObject.SetActive(isBusy);
+    }
+
+}
