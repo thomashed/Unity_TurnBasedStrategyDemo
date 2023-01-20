979efcf46531e4da96011a7da464bc8bf051f704
Thomas <thomashj93@gmail.com>
2023-01-20 17:06:59 +0100

    Add vfx to projectile: curve on Y to have arc, explosion and new material

--- Assets/Scripts/GrenadeProjectile.cs
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -8,24 +8,43 @@ public class GrenadeProjectile : MonoBehaviour
 {
     public static event EventHandler AnyGrenadeExploded;
 
+    [SerializeField] private Transform grenadeExplodeVfxPrefab;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private AnimationCurve arcYAnimation;
+
     private Vector3 targetPosition;
     private int damageEffect = 30;
-    private Action onGrenadeBehaviourComplete; 
+    private Action onGrenadeBehaviourComplete;
+    
+    private float totalDistance;
+    private Vector3 positionXZ; 
+
 
     public void Setup(GridPosition targetGridPosition, Action onGrenadeBehaviourComplete)
     {
         targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
         this.onGrenadeBehaviourComplete = onGrenadeBehaviourComplete;
+
+        positionXZ = transform.position;
+        positionXZ.y = 0;
+        totalDistance = Vector3.Distance(positionXZ, targetPosition);  
     }
 
     private void Update()
     {
-        Vector3 moveDir = (targetPosition - transform.position).normalized;
+        Vector3 moveDir = (targetPosition - positionXZ).normalized;
         float moveSpeed =15f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-    
+        positionXZ += moveDir * moveSpeed * Time.deltaTime;
+
+        float distance = Vector3.Distance(positionXZ, targetPosition);
+        float distanceNormalized = 1 - distance / totalDistance; // invert
+
+        float maxHeight = totalDistance / 4f;
+        float positionY = arcYAnimation.Evaluate(distanceNormalized) * maxHeight;
+        transform.position = new Vector3(positionXZ.x, positionY, positionXZ.z);
+
         float reachedTargetDistance = .2f;
-        if (Vector3.Distance(targetPosition, transform.position) < reachedTargetDistance)
+        if (Vector3.Distance(targetPosition, positionXZ) < reachedTargetDistance)
         {
             float damageRadius = 4f;
             var colliderArray = Physics.OverlapSphere(targetPosition, damageRadius);
@@ -37,6 +56,8 @@ public class GrenadeProjectile : MonoBehaviour
             }
 
             OnAnyGrenadeExploded();
+            trailRenderer.transform.parent = null;
+            Instantiate(grenadeExplodeVfxPrefab, targetPosition + Vector3.up * 1f, Quaternion.identity);
             Destroy(gameObject);
             onGrenadeBehaviourComplete();
         }
